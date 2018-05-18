212ea121e869b35091f74badfb52eab97a004d78
Eric Murta <ericmurta@hotmail.com>
2018-05-17 22:13:41 -0300

    Add counterUs field

--- graphql/Schema/Schema.js
+++ graphql/Schema/Schema.js
@@ -26,8 +26,41 @@ const queryType = new GraphQLObjectType({
 				})
 				return heroInfo;	
 			}
+		},
+		counterUs: {
+			type: GraphQLList(GraphQLString),
+			args: {
+				heroes: { type: GraphQLList(GraphQLString) },
+			},
+			resolve: (_, { heroes }) => {
+				const counters = Promise.all(
+					heroes.map(h=> Model.findOne({ 'name': h }, { 'bestAgaints': 0 }, function(err, counters) {
+						return new Promise((resolve, reject) => {
+							if (err) {
+								reject(err);
+							} else {
+								resolve(counters);
+							}
+						});
+					}))
+				).then(counters => {
+						const worstAgaints = counters.map(counter => counter.worstAgaints);
+						if (worstAgaints.length > 1) {
+							const [hdWorstAgaints] = worstAgaints.splice(0,1);
+							const myCounters = hdWorstAgaints.filter(hero => {
+								return worstAgaints.every(counters => {
+									return !!~counters.indexOf(hero)
+								})
+							});
+							return myCounters;
+						} else {
+							return worstAgaints[0];
+						}
+					});	
+					return counters;
+				},
+			},
 		}
-	}
 })
 
 const schema = new GraphQLSchema({ query: queryType });
