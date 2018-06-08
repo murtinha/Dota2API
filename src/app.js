d54bf679c8fecdb86576d8484494ee42006671b3
Eric Murta <ericmurta@hotmail.com>
2018-06-07 23:54:58 -0300

    Use dao on rest api

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import Model from './models/model';
 import schema from '../graphql/Schema/Schema';
 import graphqlHTTP from 'express-graphql';
+import HeroDAO from './dao/hero-dao';
 
 mongoose.connect('mongodb://localhost:27017/heroesdb', { autoIndex: false });
 
@@ -27,103 +28,35 @@ app.get('/', function (req, res) {
 	res.send("WELCOME TO DOTA 2 COUNTER ME");
 });
 
-app.get('/hero/:name', function(req, res) {
+app.get('/hero/:name', async function(req, res) {
 	const name = req.params.name;
-	Model.findOne({'name': name}, function(err, hero) {
-		if (err) return handleError(err);
-		res.json(hero)
-	});
+	const hero = await HeroDAO.findOne({'name': name})
+  res.json(hero)
 });
 
-app.get('/counter-me/:name', function(req,res) {
+app.get('/counter-me/:name', async function(req,res) {
 	const name = req.params.name;
-	Model.findOne({'name': name}, function(err, hero) {
-		if (err) return handleError(err);
-		res.json(hero.worstAgaints)
-	});
+	const counters = await HeroDAO.findMyCounters({'name': name}, 'worstAgaints')
+  res.json(counters);
 });
 
-app.get('/counter-us/:heroes', function(req, res) {
+app.get('/counter-us/:heroes', async function(req, res) {
 	const heroes = req.params.heroes.split(',');
-	Promise.all(
-		heroes.map(h=> Model.findOne({ 'name': h }, { 'bestAgaints': 0 }, function(err, counters) {
-			return new Promise((resolve, reject) => {
-				if (err) {
-					reject(err);
-				} else {
-					resolve(counters);
-				}
-			});
-		}))
-	).then(counters => {
-		const worstAgaints = counters.map(counter => counter.worstAgaints);
-		if (worstAgaints.length > 1) {
-			const [hdWorstAgaints] = worstAgaints.splice(0,1);
-			const myCounters = hdWorstAgaints.filter(hero => {
-				return worstAgaints.every(counters => {
-					return !!~counters.indexOf(hero)
-				})
-			});
-			res.json(myCounters)
-		} else {
-			res.json(...worstAgaints)
-		}
-	});	
+  const counters = await HeroDAO.findCounters(heroes, { 'bestAgaints': 0 }, 'worstAgaints');
+  res.json(counters)
 });
 
-app.get('/i-counter/:name', function(req,res) {
+app.get('/i-counter/:name', async function(req,res) {
 	const name = req.params.name;
-	Model.findOne({'name': name}, function(err, hero) {
-		if (err) return handleError(err);
-		res.json(hero.bestAgaints)
-	});
+	const counters = await HeroDAO.findMyCounters({'name': name}, 'bestAgaints')
+  res.json(counters);
 });
 
-app.get('/we-counter/:heroes', function(req, res) {
+app.get('/we-counter/:heroes', async function(req, res) {
 	const heroes = req.params.heroes.split(',');
-	Promise.all(
-		heroes.map(h=> Model.findOne({ 'name': h }, { 'worstAgaints': 0 }, function(err, counters) {
-			return new Promise((resolve, reject) => {
-				if (err) {
-					reject(err);
-				} else {
-					resolve(counters);
-				}
-			});
-		}))
-	).then(counters => {
-		const bestAgaints = counters.map(counter => counter.bestAgaints);
-		if (bestAgaints.length > 1) {
-			const [hdBestAgaints] = bestAgaints.splice(0,1);
-			const myCounters = hdBestAgaints.filter(hero => {
-				return bestAgaints.every(counters => {
-					return !!~counters.indexOf(hero)
-				})
-			});
-			res.json(myCounters)
-		} else {
-			res.json(...bestAgaints)
-		}
-	});	
+  const counters = await HeroDAO.findCounters(heroes, { 'bestAgaints': 0 }, 'bestAgaints');
+  res.json(counters)
 });
-
-// FGOD solution
-// app.get('/counter-us/:heroes', async function(req, res) {
-// 	const heroes = req.params.heroes.split(',');
-// 	const results = await Promise.all(
-// 			heroes.map( h => Model.findOne({ 'name': h }, { 'bestAgaints': 0 }, function(err, counters) {
-// 				return new Promise((resolve, reject) => {
-// 					if (err) {
-// 						reject(err);
-// 					} else {
-// 						console.log(counters.worstAgaints)
-// 						resolve(counters);
-// 					}
-// 				});
-// 			}))
-// 	);
-// 	res.send(results)
-// });
 
 app.listen(3000, function() {
 	console.log("Server running on port 3000 with graphql api on /graphql");
